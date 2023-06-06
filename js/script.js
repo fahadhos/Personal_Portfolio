53c2bbb1f9b8943e2af30d892f15b9bf4de1e74d
fahadhos <generalus159@gmail.com>
2023-06-06 17:55:30 +0600

    I have added EmailJs Auto reply system in contact form

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ let menuicon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 
-menuicon.onclick = () =>{
-menuicon.classList.toggle('bx-x');
-navbar.classList.toggle('active');
+menuicon.onclick = () => {
+    menuicon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 
 };
 
@@ -19,18 +19,18 @@ let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a ');
 
 
-window.onscroll = ()=>{
+window.onscroll = () => {
 
-    sections.forEach(sec =>{
+    sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -150;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-        if( top >= offset && top < offset +height){
-        navlinks.forEach(links=>{
-            links.classList.remove('active');
-            document.querySelector('header nav a[href*=' +id +']').classList.add('active')
-        });
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            });
         };
     });
 
@@ -38,7 +38,7 @@ window.onscroll = ()=>{
 
     let header = document.querySelector('header');
 
-    header.classList.toggle('sticky',window.scrollY>100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
 
     // ==== remove toggle icon and navbar when click navbar link (scroll) =======//
@@ -48,3 +48,40 @@ window.onscroll = ()=>{
 };
 
 
+
+
+
+
+function sendmail() {
+    var params = {
+
+        name: document.getElementById('name').value,
+
+        email: document.getElementById('email').value,
+
+        phone: document.getElementById('phone').value,
+
+        subject: document.getElementById('subject').value,
+
+        msg: document.getElementById('msg').value,
+
+
+
+
+    };
+
+    const serviceid = "service_p91amrr";
+    const templateid = "template_rj3r7bp";
+
+    emailjs.send(serviceid, templateid, params).then(
+        res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("phone").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("msg").value = "";
+            console.log(res);
+            alert("your message sent successfully");
+
+        }).catch((err) => console.log(err));
+}
