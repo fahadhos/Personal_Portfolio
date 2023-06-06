5db486ca3584765a2a15992b85d0c3188edbcd12
fahadhos <generalus159@gmail.com>
2023-06-06 14:54:56 +0600

    this contain Javascript coding

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+// ==== toggle icon navbar
+
+let menuicon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+menuicon.onclick = () =>{
+menuicon.classList.toggle('bx-x');
+navbar.classList.toggle('active');
+
+};
+
+
+// scroll section
+
+
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a ');
+
+
+window.onscroll = ()=>{
+
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if( top >= offset && top < offset +height){
+        navlinks.forEach(links=>{
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' +id +']').classList.add('active')
+        });
+        };
+    });
+
+    // Sticky navbar
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky',window.scrollY>100);
+
+
+    // ==== remove toggle icon and navbar when click navbar link (scroll) =======//
+
+    menuicon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
